ca21d8b1d68f2cd4e90ef350852ff4a6d85fb2b7
takbeom <tak@yachtdesignworks.com>
2020-05-28 08:25:15 -0400

    39CC 360 update

--- 360 Panoramic/39 CC/app-files/data.js
+++ 360 Panoramic/39 CC/app-files/data.js
@@ -16,73 +16,24 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 3.134363598552305,
-        "pitch": 0.40106791936434405,
+        "yaw": 3.1205612065537656,
+        "pitch": 0.7291062570998186,
         "fov": 1.4256478217424775
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.0749061689098518,
-          "pitch": 0.012870153350947433,
-          "rotation": 0,
-          "target": "1-helm"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
       "id": "1-helm",
       "name": "HELM",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "yaw": 3.1193279780642964,
-        "pitch": 0.57777209208502,
-        "fov": 1.4256478217424775
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.413545748259031,
-          "pitch": 0.510821106207807,
-          "rotation": 0,
-          "target": "2-head-line"
-        },
-        {
-          "yaw": 3.0742859289903848,
-          "pitch": 0.08128148647719158,
-          "rotation": 0,
-          "target": "0-bow"
-        },
-        {
-          "yaw": 0.06533717281832274,
-          "pitch": 0.3938966069057166,
-          "rotation": 0,
-          "target": "3-transom"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-head-line",
-      "name": "HEAD LINE",
       "levels": [
         {
           "tileSize": 256,
@@ -104,15 +55,34 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 3.1021608792117874,
+        "pitch": 0.477886062129695,
+        "fov": 1.4256478217424775
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 3.069660200651021,
+          "pitch": 0.11028638363570309,
+          "rotation": 0,
+          "target": "2-transom"
+        },
+        {
+          "yaw": 2.4169839537890976,
+          "pitch": 0.4954129618627814,
+          "rotation": 0,
+          "target": "3-interior"
+        },
+        {
+          "yaw": 0.07403658801163537,
+          "pitch": 0.39345250958065847,
+          "rotation": 0,
+          "target": "2-transom"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "3-transom",
+      "id": "2-transom",
       "name": "TRANSOM",
       "levels": [
         {
@@ -127,18 +97,60 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 3.104181214064538,
+        "pitch": 0.4094692444898307,
+        "fov": 1.4256478217424775
       },
       "linkHotspots": [
         {
-          "yaw": -3.0608898370598414,
-          "pitch": 0.05917203730344056,
+          "yaw": -3.0893611970010166,
+          "pitch": 0.003062897758823624,
+          "rotation": 0,
+          "target": "1-helm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-interior",
+      "name": "INTERIOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7126385357874216,
+        "pitch": 0.03754278532664301,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.507568789333499,
+          "pitch": -0.15801602292633632,
           "rotation": 0,
           "target": "1-helm"
         }
@@ -146,11 +158,11 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "BERTRAM 39CC",
+  "name": "BERTRAM 39 CC",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
