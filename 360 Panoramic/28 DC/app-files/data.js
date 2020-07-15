49ea7e7163ba41de88ca1f5146d299e53d770d19
takbeom <tak@yachtdesignworks.com>
2020-07-15 15:21:31 -0400

    28_DC Panoramic

---
+++ 360 Panoramic/28 DC/app-files/data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-transom",
+      "name": "Transom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5973657192095514,
+        "pitch": 0.33852938837205926,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5466896766491818,
+          "pitch": 0.1882685235976318,
+          "rotation": 0,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 1.8961009460015656,
+          "pitch": 0.21939089082878027,
+          "rotation": 0,
+          "target": "1-helm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-helm",
+      "name": "Helm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5741845126510334,
+        "pitch": 0.3804688363496531,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0666454526392037,
+          "pitch": 0.4536442521921611,
+          "rotation": 0,
+          "target": "2-bow"
+        },
+        {
+          "yaw": -1.2441044294781651,
+          "pitch": 0.561122902119088,
+          "rotation": 0,
+          "target": "0-transom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5998813566758345,
+        "pitch": 0.4395866572709153,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1354180799179545,
+          "pitch": 0.2460583882404741,
+          "rotation": 0,
+          "target": "1-helm"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bertram 28 DC_Working",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
